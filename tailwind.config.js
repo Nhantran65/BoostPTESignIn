46900c0af6892cb1a967a0c98e1dfcc69e6bfe4c
Nhan Tran <rim25102004@gmail.com>
2023-06-13 21:42:40 +0700

    Add New Flatform of BoostPTE Sign In

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,9 @@ module.exports = {
   theme: {
     extend: {
       spacing: {
+        42: '4px',
+        0.05: '0.05em',
+        1: '1px',
         35: "35px",
         450: "450px",	
         514: "514px",
@@ -14,18 +17,26 @@ module.exports = {
         15: "15px",
         5: "5px",
         6: "6px",
+        21: "21px",
+        0.9: "0.9px",
+        340: "340px",
+        28.8: "28.8px",
       },
       colors: {
         gray33: "#333",
         orangefa: "#ffa400",
       },
       borderRadius: {
+        '50': "50px",
         '30': '30px',
       },
       fontSize: {
         '32': '32px',
         '14': '14px',
       },
+      borderWidth: {
+        1: '1px',
+      }
     },
   },
   variants: {
